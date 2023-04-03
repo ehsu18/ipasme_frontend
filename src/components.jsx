d87ec585764c084123ef1415be021059f6b60b89
Jhojana Gomez <118693498+kjgomez26programation@users.noreply.github.com>
2023-04-03 17:05:59 -0500

    Add files via upload

--- src/components.jsx
+++ src/components.jsx
@@ -1,14 +1,48 @@
 // import './styles.css'
 import logoIpasme from './assets/logo-ipasme.png'
 import inicioIcon from './assets/inicio.png'
+import Notificaciones from './assets/bell.png'
+import verhistorias from './assets/verhistoria.png'
+import crearhistoria from './assets/nuevahistoria.png'
+import estadisticas from './assets/stats.png'
+import configuracion from './assets/config.png'
+import ayuda from  './assets/ayuda.png'
+import user from './assets/user.png'
 
 function Componente() {
     return <nav className="navbar">
         <div className='nav-logo-container'><img src={logoIpasme} alt="" /></div>
+        <ul className='micro-italic'>
+            <li>Navegar</li>
+        </ul>
         <ul>
             <TabOption selected={true} text='Inicio' icon={inicioIcon} />
+            <TabOption selected={true} text='Notificaciones' icon={Notificaciones}/>
+            <TabOption selected={true} text='Ver Historias' icon={verhistorias}/>
+            <TabOption selected={true} text='Crear Historia' icon={crearhistoria}/>
+            <TabOption selected={true} text='Estadisticas' icon={estadisticas}/>
+
         </ul>
-    
+        <hr/>
+        <ul className='micro-italic'>
+            <li>Opciones</li>
+        </ul>
+        <ul>
+            <TabOption selected={true} text='configuracion' icon={configuracion}/>
+            <TabOption selected={true} text='Ayuda' icon={ayuda}/>
+        </ul>
+
+       <hr />
+
+        <div className='user-card'>
+            <img src={user} alt="" />
+            <div className='usuario-info'>
+                <small className='title-small'>Lorena Chacón</small>
+                <small className='paragraph-small'>Usuario Estandar</small>
+            </div>
+        </div>
+
+
     </nav>
 }
 
--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom/client';
 import Componente from './components'
 import "./styles.css"
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Componente/>
+
+
+
 );
 
