34ce1b54db78d1f4719c9190ed36447696ece23e
Elian Sumalave <esumalave44@gmail.com>
2023-07-08 16:51:58 -0400

    maquetada la seccion de datos personales

--- src/components/RecordDetailsPage.jsx
+++ src/components/RecordDetailsPage.jsx
@@ -6,17 +6,18 @@ import { useEffect, useState } from "react";
 
 export function RecordDetailsPage() {
   let { id } = useParams();
-  const [ recordBasic, configRecordBasic ] = useState({});
+  const [recordBasic, configRecordBasic] = useState({});
 
-  useEffect(()=>{getRecords(id)
-    .then((response) => response.json())
-    .then((json) => {
-      configRecordBasic(json);
-    })
-    .catch((error) => {
-      throw new Error(error);
-    })
-  }, [id])
+  useEffect(() => {
+    getRecords(id)
+      .then((response) => response.json())
+      .then((json) => {
+        configRecordBasic(json);
+      })
+      .catch((error) => {
+        throw new Error(error);
+      });
+  }, [id]);
 
   return (
     <main className="main-container">
@@ -36,30 +37,36 @@ export function RecordDetailsPage() {
           />
         </div>
       </header>
+      {/* TODO revisar si no era content-container */}
       <div className="recorddetails-container">
         <div className="recorddetails common-container flex-v">
-          
           <header className="recorddetails-header flex-h">
-
             <section className="left flex-h">
-              <div style={ recordBasic['type']==='beneficiary' ?
-                { backgroundColor: "var(--main-beneficiario)" } :
-                { backgroundColor: "var(--main-afiliado)" }}
-                className="accent"></div>
+              <div
+                style={
+                  recordBasic["type"] === "beneficiary"
+                    ? { backgroundColor: "var(--main-beneficiario)" }
+                    : { backgroundColor: "var(--main-afiliado)" }
+                }
+                className="accent"
+              ></div>
 
               <div className="felx-v">
                 <div className="title flex-h">
-                  <span className="title-big">{recordBasic.name + ' ' + recordBasic.lastname}</span>
-                  <PersonTypeTag type={recordBasic['type']} />
+                  <span className="title-big">
+                    {recordBasic.name + " " + recordBasic.lastname}
+                  </span>
+                  <PersonTypeTag type={recordBasic["type"]} />
                 </div>
                 <span className="title-regular">{recordBasic.document}</span>
               </div>
-
             </section>
 
             <section className="right flex-v">
-              <div className=" flex-h" style={{gap: "8px"}}>
-                <span className="title-regular">Ubicaci&oacute;n de la carpeta</span>
+              <div className=" flex-h" style={{ gap: "8px" }}>
+                <span className="title-regular">
+                  Ubicaci&oacute;n de la carpeta
+                </span>
                 {icons.Edit4(20)}
               </div>
               <p>Este texto es solo de ejemplo y no deberia verse</p>
@@ -67,114 +74,145 @@ export function RecordDetailsPage() {
             </section>
           </header>
           <nav className="flex-h recorddetails-nav">
-            <div className="title-small recorddetails-tab recorddetails-tab-selected"><span>Datos personales</span></div>
-            <div className="title-small recorddetails-tab"><span>Datos m&eacute;dicos</span></div>
-            <div className="title-small recorddetails-tab"><span>Datos odontol&oacute;gicos</span></div>
-            <div className="title-small recorddetails-tab"><span>Reposos y cuidos</span></div>
-            <div className="title-small recorddetails-tab"><span>Opciones</span></div>
+            <div className="title-small recorddetails-tab recorddetails-tab-selected">
+              <span>Datos personales</span>
+            </div>
+            <div className="title-small recorddetails-tab">
+              <span>Datos m&eacute;dicos</span>
+            </div>
+            <div className="title-small recorddetails-tab">
+              <span>Datos odontol&oacute;gicos</span>
+            </div>
+            <div className="title-small recorddetails-tab">
+              <span>Reposos y cuidos</span>
+            </div>
+            <div className="title-small recorddetails-tab">
+              <span>Opciones</span>
+            </div>
           </nav>
 
-          <section className="recorddetails-section">
-            <header className="felx-h">
-              {icons.User1(24)}
-              <span className="title-regular">Datos personales</span>
-            </header>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-disabled paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-selected paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-blocked paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-active paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="flex-h recorddetails-section-info">
-              <span className="micro-italic">Realice los cambios necesarios y luego haga click en guardar</span>
-              <ButtonBig text="Editar" icon={icons.DocumentEdit} type='main' />
-          </div>
-          </section>
-          <section className="recorddetails-section">
-            <header className="felx-h">
-              {icons.User1(24)}
-              <span className="title-regular">Datos personales</span>
-            </header>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-disabled paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-selected paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-blocked paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-active paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-
-          </section>
-          <section className="recorddetails-section">
-            <header className="felx-h">
-              {icons.User1(24)}
-              <span className="title-regular">Datos personales</span>
-            </header>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-disabled paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-selected paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-blocked paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-active paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-
-          </section>
-          <section className="recorddetails-section">
-            <header className="felx-h">
-              {icons.User1(24)}
-              <span className="title-regular">Datos personales</span>
-            </header>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-disabled paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-selected paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-blocked paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-            <div className="recorddetails-section-datacontainer">
-              <span className="micro-italic">C&eacute;dula</span>
-              <input className="entry-1-active paragraph-regular" type="text" value={"29.730.724"}/>
-            </div>
-
-          </section>
-
-          
-
+          <RecordDetailsSection title="Datos personales">
+            <RecordDetailsDataContainer
+              label="Cedula"
+              name="document"
+              data={recordBasic["document"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Nacionalidad"
+              name="nationality"
+              data={recordBasic["nationality"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Nombres"
+              name="name"
+              data={recordBasic["name"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Apellidos"
+              name="lastname"
+              data={recordBasic["lastname"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Fecha de nacimiento"
+              name="dateofbirth"
+              data={recordBasic["dateofbirth"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Estado civil"
+              name="civilstatus"
+              data={recordBasic["civilstatus"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Lugar de nacimiento"
+              name="placeofbirth"
+              data={recordBasic["placeofbirth"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Sexo"
+              name="sex"
+              data={recordBasic["sex"] || "indefinido"}
+            />
+          </RecordDetailsSection>
+          <RecordDetailsSection title="Datos de contacto">
+            <RecordDetailsDataContainer
+              label="Telefono personal"
+              name="personalphone"
+              data={recordBasic["personalphone"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Telefono opcional"
+              name="optionalphone"
+              data={recordBasic["optionalphone"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Direccion de habitacion"
+              name="direction"
+              data={recordBasic["direction"] || "indefinido"}
+              doubleColumn
+            />
+          </RecordDetailsSection>
+          {recordBasic['type'] == 'affiliate' ? 
+            <RecordDetailsSection title='Datos laborales' >
+              <RecordDetailsDataContainer
+              label="Estado laboral"
+              name="jobstatus"
+              data={recordBasic["jobstatus"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Cargo"
+              name="jobtitle"
+              data={recordBasic["jobtitle"] || "indefinido"}
+            />
+            <RecordDetailsDataContainer
+              label="Direccion del plantel"
+              name="jobdirection"
+              data={recordBasic["jobdirection"] || "indefinido"}
+              doubleColumn
+            />
+            </RecordDetailsSection>
+          : null}
         </div>
-        
       </div>
     </main>
+  );
+}
+
+function RecordDetailsSection({ title, children }) {
+  return (
+    <section className="recorddetails-section">
+      <header className="felx-h">
+        {icons.User1(24)}
+        <span className="title-regular">{title}</span>
+      </header>
+
+      {children}
+
+      <div className="flex-h recorddetails-section-info">
+        <span className="micro-italic">
+          Realice los cambios necesarios y luego haga click en guardar
+        </span>
+        <ButtonBig text="Editar" icon={icons.DocumentEdit} type="main" />
+      </div>
+    </section>
+  );
+}
+
+function RecordDetailsDataContainer({ label, data, name, doubleColumn }) {
+  // let [state, setState] = useState();
+
+  // readonly, active, blocked, selected ... soon -> hover, error, warning
+  return (
+    <div
+      className="recorddetails-section-datacontainer"
+      style={doubleColumn ? { gridColumn: "span 2" } : {}}
+    >
+      <span className="micro-italic">{label}</span>
+      <input
+        className={"entry-1-" + "readonly" + " paragraph-regular"}
+        type={label}
+        name={name}
+        value={data}
+      />
+    </div>
   );
 }
